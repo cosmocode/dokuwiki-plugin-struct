4617d1b6ad1566d397f58c36102c4f9f1af8396c
Christoph Wanasek <christoph.wanasek@hotmail.com>
2018-12-21 20:35:35 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Christoph Wanasek <christoph.wanasek@hotmail.com>
  * @author Jürgen <hans-juergen.schuemmer@schuette.de>
  * @author Joerg <scooter22@gmx.de>
  * @author Andreas Gohr <andi@splitbrain.org>
@@ -13,24 +14,24 @@ $lang['menu_assignments']      = 'Struct Schema Zuweisungen';
 $lang['headline']              = 'Strukturierte Daten';
 $lang['page schema']           = 'Seiten Schema';
 $lang['lookup schema']         = 'Lookup Schema';
-$lang['edithl page']           = 'Bearbeitung Seiten Schema <i>%s</i>';
-$lang['edithl lookup']         = 'Bearbeitung Lookup  Schema <i>%s</i>';
+$lang['edithl page']           = 'Bearbeite Seiten Schema <i>%s</i>';
+$lang['edithl lookup']         = 'Bearbeite Lookup Schema <i>%s</i>';
 $lang['create']                = 'Neues Schema anlegen';
 $lang['schemaname']            = 'Schema-Name:';
 $lang['save']                  = 'Speichern';
 $lang['createhint']            = 'Achtung: Schemas können später nicht umbenannt werden';
 $lang['pagelabel']             = 'Seite';
-$lang['rowlabel']              = 'Reihe #';
+$lang['rowlabel']              = 'Zeile #';
 $lang['revisionlabel']         = 'Zuletzt geändert';
 $lang['userlabel']             = 'letzter Bearbeitender';
 $lang['summarylabel']          = 'Letzte Zusammenfassung';
-$lang['summary']               = 'Struct-Daten geändert';
+$lang['summary']               = 'Strukturierte Daten geändert';
 $lang['export']                = 'Schema als JSON exportieren';
 $lang['btn_export']            = 'Exportieren';
 $lang['import']                = 'Importieren eines Schemas aus JSON';
 $lang['btn_import']            = 'Importieren';
 $lang['import_warning']        = 'Achtung: dies überschriebt bereits definierte Felder!';
-$lang['del_confirm']           = 'Namen des Schema zur Bestätigung der Löschung eingeben';
+$lang['del_confirm']           = 'Namen des Schemas zur Bestätigung der Löschung eingeben';
 $lang['del_fail']              = 'Die Schemanamen stimmten nicht überein. Schema nicht gelöscht';
 $lang['del_ok']                = 'Schema wurde gelöscht';
 $lang['btn_delete']            = 'Löschen';
@@ -47,16 +48,16 @@ $lang['editor_sort']           = 'Sortieren';
 $lang['editor_label']          = 'Feldname';
 $lang['editor_multi']          = 'Mehrfach-Eingabe?';
 $lang['editor_conf']           = 'Konfiguration';
-$lang['editor_type']           = 'Eingeben';
+$lang['editor_type']           = 'Typ';
 $lang['editor_enabled']        = 'Aktiviert';
-$lang['editor_editors']        = 'Kommagetrennte Liste von Benutzern und @groups, welche die Daten dieses Schemas bearbeiten können (leer für alle) :.';
+$lang['editor_editors']        = 'Kommagetrennte Liste von Benutzern und @groups, welche die Daten dieses Schemas bearbeiten können (leer für alle):';
 $lang['assign_add']            = 'Hinzufügen';
 $lang['assign_del']            = 'Löschen';
 $lang['assign_assign']         = 'Seiten/Namensraum';
 $lang['assign_tbl']            = 'Schema';
 $lang['multi']                 = 'Geben Sie mehrere, durch Kommas getrennte Werte ein.';
 $lang['multidropdown']         = 'Halte STRG oder CMD um mehrere Werte auszuwählen.';
-$lang['duplicate_label']       = 'Label <code>%s </code> existiert bereits im Schema, zweites Vorkommen wurde in <code>%s</ code> umbenannt.';
+$lang['duplicate_label']       = 'Label <code>%s</code> existiert bereits im Schema, zweites Vorkommen wurde in <code>%s</ code> umbenannt.';
 $lang['emptypage']             = 'Strukturdaten wurden nicht für eine leere Seite gespeichert';
 $lang['validation_prefix']     = 'Feld [%s]: ';
 $lang['Validation Exception Decimal needed'] = 'es sind nur Dezimalzahlen erlaubt';
@@ -72,20 +73,23 @@ $lang['Validation Exception bad color specification'] = 'muss im Format \'\'#RRG
 $lang['Exception illegal option'] = 'Die Option \'<code>%s</code>\' ist für diesen Aggregationstyp ungültig.';
 $lang['Exception noschemas']   = 'Keine Schemas für das Laden von Spalten angegeben';
 $lang['Exception nocolname']   = 'Kein Spaltenname angegeben';
+$lang['Exception nolookupmix'] = 'Du kannst nicht mehr als ein Lookup aggregieren oder es mit Seiten-Daten mischen';
+$lang['Exception nolookupassign'] = 'Due kannst keien Lookup Schemas zu Seiten zuordnen';
 $lang['Exception No data saved'] = 'Keine Daten gespeichert';
 $lang['Exception no sqlite']   = 'Das \'Struct Plugin\' benötigt das \'Sqlite Plugin\'. Bitte installieren und aktivieren.';
+$lang['Exception column not in table'] = 'Es existiert keine Spalte %s im Schema %s.';
 $lang['Warning: no filters for cloud'] = 'Filter werden in \'Struct Clouds\' nicht unterstützt';
 $lang['sort']                  = 'Nach dieser Spalte sortieren';
 $lang['next']                  = 'Nächste Seite';
 $lang['prev']                  = 'Vorherige Seite';
 $lang['none']                  = 'Nichts gefunden';
 $lang['csvexport']             = 'CSV-Export';
-$lang['admin_csvexport']       = 'Exportieren von Rohdaten in einer CSV-Datei';
-$lang['admin_csvimport']       = 'Importieren von Rohdaten aus einer CSV-Datei';
+$lang['admin_csvexport']       = 'Exportiere Rohdaten in eine CSV-Datei';
+$lang['admin_csvimport']       = 'Importiere Rohdaten aus einer CSV-Datei';
 $lang['admin_csvdone']         = 'CSV-Datei importiert';
 $lang['admin_csvhelp']         = 'Bitte konsultieren Sie das Handbuch zum CSV-Import (engl.) für Formatierungsdetails.';
-$lang['tablefilteredby']       = 'Filterung mit %s';
-$lang['tableresetfilter']      = 'Zeige alle (Filter/Sortierung löschen)';
+$lang['tablefilteredby']       = 'Gefiltert nach %s';
+$lang['tableresetfilter']      = 'Alles zeigen (Filter/Sortierung löschen)';
 $lang['Exception schema missing'] = 'Schema %s existiert nicht!';
 $lang['no_lookup_for_page']    = 'Sie können den Lookup Editor nicht bei einem Seiten-Schema benutzen!';
 $lang['lookup new entry']      = 'Neuen Eintrag anlegen';
