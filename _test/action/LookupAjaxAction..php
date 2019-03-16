dokuwiki/dokuwiki

commit ff2afc7c7bdf9f04b6bc2917af42ff865757c54d

refactor: Move saving of lookup row to helper method
This should make saving lookup data easier, both for tests and external plugins

action/lookup.php
@@ -106,19 +106,12 @@ class action_plugin_struct_lookup extends DokuWiki_Action_Plugin {
         $data = $INPUT->arr('entry');
         action_plugin_struct_inline::checkCSRF();
 
-        $access = AccessTable::byTableName($tablename, 0, 0);
-        if(!$access->getSchema()->isEditable()) {
-            throw new StructException('lookup save error: no permission for schema');
-        }
-        $validator = $access->getValidator($data);
-        if(!$validator->validate()) {
-            throw new StructException("Validation failed:\n%s", join("\n", $validator->getErrors()));
-        }
-        if(!$validator->saveData()) {
-            throw new StructException('No data saved');
-        }
+        /** @var helper_plugin_struct $helper */
+        $helper = plugin_load('helper', 'struct');
+        $helper->saveLookupData($tablename, $data);
 
         // create a new row based on the original aggregation config for the new pid
+        $access = AccessTable::byTableName($tablename, 0, 0);
         $pid = $access->getPid();
         $config = json_decode($INPUT->str('searchconf'), true);
         $config['filter'] = array(array('%rowid%', '=', $pid, 'AND'));

helper.php
@@ -104,6 +104,27 @@ class helper_plugin_struct extends DokuWiki_Plugin {
     }
 
     /**
+     * Save data row for a lookup schema
+     *
+     * @param string $tablename the name of the lookup schema into which to save the data
+     * @param array  $data data to be saved in the form of [columnName => 'data']
+     */
+    public function saveLookupData($tablename, $data)
+    {
+        $access = AccessTable::byTableName($tablename, 0, 0);
+        if(!$access->getSchema()->isEditable()) {
+            throw new StructException('lookup save error: no permission for schema');
+        }
+        $validator = $access->getValidator($data);
+        if(!$validator->validate()) {
+            throw new StructException("Validation failed:\n%s", implode("\n", $validator->getErrors()));
+        }
+        if(!$validator->saveData()) {
+            throw new StructException('No data saved');
+        }
+    }
+
+    /**
      * Creates a new page revision with the same page content as before
      *
      * @param string $page

cosmocode/dokuwiki-plugin-struct

commit f0ee60b954b9647d3eb2249b991590b08ad39158

🐛 Fix saved lookup row not being displayed
This fixes a bug that was introduced in ff2afc7c7bdf9f04b6bc2917af42ff865757c54d

When saving the lookup we need the id of the row we just saved to create
the response to the original ajax request.

Closes #398

_test/action/LookupAjaxAction..php
@@ -1,0 +1,52 @@
+<?php
+
+namespace dokuwiki\plugin\struct\test\action;
+
+use dokuwiki\plugin\struct\meta\AccessTable;
+use dokuwiki\plugin\struct\test\StructTest;
+
+/**
+ * @covers action_plugin_struct_lookup
+ *
+ * @group plugin_struct
+ * @group plugins
+ * @group integration
+ */
+class LookupAjaxAction extends StructTest
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->loadSchemaJSON('wikilookup', '', 0, true);
+
+        /** @var \helper_plugin_struct $helper */
+        $helper = plugin_load('helper', 'struct');
+
+        $saveDate = [
+            'FirstFieldText' => 'abc def',
+            'SecondFieldLongText' => "abc\ndef\n",
+            'ThirdFieldWiki' => "  * hi\n  * ho",
+        ];
+        $access = AccessTable::byTableName('wikilookup', 0, 0);
+        $helper->saveLookupData($access, $saveDate);
+    }
+
+    public function testSaveLookupDataEvent()
+    {
+        $testLabel = 'testcontent';
+        global $INPUT;
+        $INPUT->post->set('schema', 'wikilookup');
+        $INPUT->post->set('entry', ['FirstFieldText' => $testLabel]);
+        $INPUT->post->set('searchconf', json_encode([
+            'schemas' => [['wikilookup', '']],
+            'cols' => ['*']
+        ]));
+        $call = 'plugin_struct_lookup_save';
+        $evt = new \Doku_Event('AJAX_CALL_UNKNOWN', $call);
+
+        $this->expectOutputRegex('/\s*<tr.*' . $testLabel . '.*<\/td>\s*/');
+
+        $evt->advise_before();
+    }
+}

action/lookup.php
@@ -111,12 +111,13 @@ class action_plugin_struct_lookup extends DokuWiki_Action_Plugin
         $data = $INPUT->arr('entry');
         action_plugin_struct_inline::checkCSRF();
 
-        /** @var helper_plugin_struct $helper */
-        $helper = plugin_load('helper', 'struct');
-        $helper->saveLookupData($tablename, $data);
-
         // create a new row based on the original aggregation config for the new pid
         $access = AccessTable::byTableName($tablename, 0, 0);
+
+        /** @var helper_plugin_struct $helper */
+        $helper = plugin_load('helper', 'struct');
+        $helper->saveLookupData($access, $data);
+
         $pid = $access->getPid();
         $config = json_decode($INPUT->str('searchconf'), true);
         $config['filter'] = array(array('%rowid%', '=', $pid, 'AND'));

helper.php
@@ -9,6 +9,7 @@
 // must be run within Dokuwiki
 use dokuwiki\plugin\struct\meta\AccessDataValidator;
 use dokuwiki\plugin\struct\meta\AccessTable;
+use dokuwiki\plugin\struct\meta\AccessTableLookup;
 use dokuwiki\plugin\struct\meta\Assignments;
 use dokuwiki\plugin\struct\meta\Schema;
 use dokuwiki\plugin\struct\meta\StructException;
@@ -106,12 +107,11 @@ class helper_plugin_struct extends DokuWiki_Plugin {
     /**
      * Save data row for a lookup schema
      *
-     * @param string $tablename the name of the lookup schema into which to save the data
-     * @param array  $data data to be saved in the form of [columnName => 'data']
+     * @param AccessTableLookup $access the table into which to save the data
+     * @param array             $data   data to be saved in the form of [columnName => 'data']
      */
-    public function saveLookupData($tablename, $data)
+    public function saveLookupData(AccessTableLookup $access, $data)
     {
-        $access = AccessTable::byTableName($tablename, 0, 0);
         if(!$access->getSchema()->isEditable()) {
             throw new StructException('lookup save error: no permission for schema');
         }

_test/AggregationExportCSV.test.php
@@ -2,6 +2,7 @@
 
 namespace dokuwiki\plugin\struct\test;
 
+use dokuwiki\plugin\struct\meta\AccessTable;
 use dokuwiki\plugin\struct\meta\ConfigParser;
 
 /**
@@ -26,7 +27,8 @@ class AggregationExportCSV extends StructTest
             'SecondFieldLongText' => "abc\ndef\n",
             'ThirdFieldWiki' => "  * hi\n  * ho",
         ];
-        $helper->saveLookupData('wikilookup', $saveDate);
+        $access = AccessTable::byTableName('wikilookup', 0, 0);
+        $helper->saveLookupData($access, $saveDate);
     }
 
     public function test_wikiColumn()