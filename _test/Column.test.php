105111a0de074250d29c24b2beb39aed5888b769
Anna Dabrowska <dabrowska@cosmocode.de>
2019-10-23 16:39:54 +0200

    Add field Increment to tests

--- _test/Column.test.php
+++ _test/Column.test.php
@@ -30,7 +30,8 @@ class column_struct_test extends StructTest {
             'Text' => 'dokuwiki\\plugin\\struct\\types\\Text',
             'Url' => 'dokuwiki\\plugin\\struct\\types\\Url',
             'User' => 'dokuwiki\\plugin\\struct\\types\\User',
-            'Wiki' => 'dokuwiki\\plugin\\struct\\types\\Wiki'
+            'Wiki' => 'dokuwiki\\plugin\\struct\\types\\Wiki',
+            'Increment' => 'dokuwiki\\plugin\\struct\\types\\Increment'
         );
 
         $this->assertEquals($expect, meta\Column::allTypes(true));
@@ -55,7 +56,8 @@ class column_struct_test extends StructTest {
             'Text' => 'dokuwiki\\plugin\\struct\\types\\Text',
             'Url' => 'dokuwiki\\plugin\\struct\\types\\Url',
             'User' => 'dokuwiki\\plugin\\struct\\types\\User',
-            'Wiki' => 'dokuwiki\\plugin\\struct\\types\\Wiki'
+            'Wiki' => 'dokuwiki\\plugin\\struct\\types\\Wiki',
+            'Increment' => 'dokuwiki\\plugin\\struct\\types\\Increment'
         );
 
         global $EVENT_HANDLER;
