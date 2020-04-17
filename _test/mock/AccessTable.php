1e11f3d953de62eea93e7ca4b557516706add6a2
Anna Dabrowska <dabrowska@cosmocode.de>
2020-04-17 21:18:26 +0200

    Consistent data type check in mock AccessTable
    The check was not updated after the mocked class was changed to handle data types instead of schema types

--- _test/mock/AccessTable.php
+++ _test/mock/AccessTable.php
@@ -3,6 +3,7 @@
 namespace dokuwiki\plugin\struct\test\mock;
 
 use dokuwiki\plugin\struct\meta;
+use dokuwiki\plugin\struct\meta\AccessTableLookup;
 use dokuwiki\plugin\struct\meta\Schema;
 
 abstract class AccessTable extends meta\AccessTable {
@@ -12,14 +13,13 @@ abstract class AccessTable extends meta\AccessTable {
      * @param int|string $pid
      * @param int $ts
      * @param int $rid
-     * @return meta\AccessTableLookup|AccessTableData
+     * @return AccessTableLookup|AccessTableData
      */
     public static function bySchema(Schema $schema, $pid, $ts = 0, $rid = 0) {
-        if(!$pid && $ts === 0) {
-            return new meta\AccessTableLookup($schema, $pid, $ts, $rid); // FIXME not mocked, yet
-        } else {
-            return new AccessTableData($schema, $pid, $ts);
+        if (self::isTypePage($pid, $ts, $rid)) {
+            return new AccessTableData($schema, $pid, $ts, $rid);
         }
+        return new AccessTableLookup($schema, $pid, $ts, $rid);
     }
 
     public static function byTableName($tablename, $pid, $ts = 0, $rid = 0) {
