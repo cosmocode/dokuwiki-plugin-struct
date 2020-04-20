95524fca9946c9419bac0a209b8bb267b204e25d
Anna Dabrowska <dabrowska@cosmocode.de>
2020-04-20 19:20:04 +0200

    Test filtering of serial data

--- _test/DataSerial.test.php
+++ _test/DataSerial.test.php
@@ -3,6 +3,7 @@
 namespace dokuwiki\plugin\struct\test;
 
 use dokuwiki\plugin\struct\meta\ConfigParser;
+use dokuwiki\plugin\struct\meta\PageMeta;
 use dokuwiki\plugin\struct\test\mock\AccessTable;
 use dokuwiki\plugin\struct\test\mock\AccessTableData;
 use dokuwiki\plugin\struct\test\mock\LookupTable;
@@ -21,9 +22,6 @@ class DataSerial_struct_test extends StructTest
     {
         parent::setUp();
         $this->loadSchemaJSON('schema1');
-
-        /** @var \helper_plugin_struct $helper */
-        $helper = plugin_load('helper', 'struct');
 
         for ($i = 0; $i < 3; $i++) {
             // save wiki pages
@@ -47,19 +45,9 @@ class DataSerial_struct_test extends StructTest
     public function test_pid()
     {
         // \syntax_plugin_struct_serial accesses the global $ID
-        $ID = 'test1';
-
-        // syntax to table
-        $syntaxConfig = ['schema: schema1', 'cols: %pageid%, %rowid%, *'];
-        $configParser = new ConfigParser($syntaxConfig);
-        $config = $configParser->getConfig();
-        // FIXME simulate addYypeFilter() from \syntax_plugin_struct_serial
-        $config['filter'][] = ['%rowid%', '!=', (string)AccessTableData::DEFAULT_PAGE_RID, 'AND'];
-        $config['filter'][] = ['%pageid%', '=', $ID, 'AND'];
-        $search = new SearchConfig($config);
-
-        $table = new LookupTable($ID, 'xhtml', new Doku_Renderer(), $search);
-        $result = $table->getResult();
+        $id = 'test1';
+        $schema = 'schema1';
+        $result = $this->fetchResult($schema, $id, []);
 
         $this->assertEquals(1, count($result));
         $this->assertEquals('test1', $result[0][0]->getValue());
@@ -68,5 +56,114 @@ class DataSerial_struct_test extends StructTest
         $this->assertEquals(['bar1', 'baz1'], $result[0][3]->getValue());
         $this->assertEquals('foobar1', $result[0][4]->getValue());
         $this->assertEquals('barfoo1', $result[0][5]->getValue());
+    }
+
+    /**
+     * Test simple text filter
+     */
+    public function test_filter_text()
+    {
+        $schema = 'schema1';
+        $result = $this->fetchResult($schema, 'test0');
+        $this->assertEquals(1, count($result));
+
+        $result = $this->fetchResult($schema, 'test0', ['first', '=', 'foo0', 'AND']);
+        $this->assertEquals(1, count($result));
+
+        $result = $this->fetchResult($schema,'test0', ['first', '!=', 'foo0', 'AND']);
+        $this->assertEquals(0, count($result));
+    }
+
+    /**
+     * Test filtering on a page field, with 'usetitles' set to true and false
+     */
+    public function test_filter_page()
+    {
+        $this->prepareLookup();
+        $schema = 'pageschema';
+        $result = $this->fetchResult($schema);
+        $this->assertEquals(3, count($result));
+
+        // 'usetitles' = true
+        $result = $this->fetchResult($schema, '', ['singletitle', '*~', 'another', 'AND']);
+        $this->assertEquals(1, count($result));
+
+        // 'usetitles' = false
+        $result = $this->fetchResult($schema, '', ['singlepage', '*~', 'this', 'AND']);
+        $this->assertEquals(0, count($result));
+    }
+
+    /**
+     * Initialize a lookup table from syntax and return the result from its internal search.
+     *
+     * @param string $schema
+     * @param string $id
+     * @param array $filters
+     * @return \dokuwiki\plugin\struct\meta\Value[][]
+     */
+    protected function fetchResult($schema, $id = '', $filters = [])
+    {
+        $syntaxConfig = ['schema: ' . $schema, 'cols: %pageid%, %rowid%, *'];
+        $configParser = new ConfigParser($syntaxConfig);
+        $config = $configParser->getConfig();
+
+        // FIXME simulate addYypeFilter() from \syntax_plugin_struct_serial or\syntax_plugin_struct_lookup
+        if ($id) {
+            $config['filter'][] = ['%rowid%', '!=', (string)AccessTableData::DEFAULT_PAGE_RID, 'AND'];
+            $config['filter'][] = ['%pageid%', '=', $id, 'AND'];
+        } else {
+            $config['filter'][] = ['%rowid%', '!=', (string)\dokuwiki\plugin\struct\meta\AccessTableData::DEFAULT_PAGE_RID, 'AND'];
+            $config['filter'][] = ['%pageid%', '=*', '^(?![\s\S])', 'AND'];
+        }
+
+        if ($filters) array_push($config['filter'], $filters);
+        $search = new SearchConfig($config);
+
+        $table = new LookupTable($id, 'xhtml', new Doku_Renderer(), $search);
+        return $table->getResult();
+    }
+
+    protected function prepareLookup()
+    {
+        saveWikiText('title1', 'test', 'test');
+        $pageMeta = new PageMeta('title1');
+        $pageMeta->setTitle('This is a title');
+
+        saveWikiText('title2', 'test', 'test');
+        $pageMeta = new PageMeta('title2');
+        $pageMeta->setTitle('This is a 2nd title');
+
+        saveWikiText('title3', 'test', 'test');
+        $pageMeta = new PageMeta('title3');
+        $pageMeta->setTitle('Another Title');
+
+        $this->loadSchemaJSON('pageschema');
+        $access = AccessTable::byTableName('pageschema', '');
+        $access->saveData(
+            array(
+                'singlepage' => 'title1',
+                'multipage' => array('title1'),
+                'singletitle' => 'title1',
+                'multititle' => array('title1'),
+            )
+        );
+        $access = AccessTable::byTableName('pageschema', '');
+        $access->saveData(
+            array(
+                'singlepage' => 'title2',
+                'multipage' => array('title2'),
+                'singletitle' => 'title2',
+                'multititle' => array('title2'),
+            )
+        );
+        $access = AccessTable::byTableName('pageschema', '');
+        $access->saveData(
+            array(
+                'singlepage' => 'title3',
+                'multipage' => array('title3'),
+                'singletitle' => 'title3',
+                'multititle' => array('title3'),
+            )
+        );
     }
 }
