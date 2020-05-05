47a35992e0ce1be4688eb244a608bd5d16351626
Anna Dabrowska <dabrowska@cosmocode.de>
2020-05-05 22:44:46 +0200

    Replace hand-crafted SQL queries in CSV import with calls to AccessTable* save methods

--- meta/CSVPageImporter.php
+++ meta/CSVPageImporter.php
@@ -2,8 +2,6 @@
 
 namespace dokuwiki\plugin\struct\meta;
 
-use dokuwiki\plugin\struct\types\Page;
-
 class CSVPageImporter extends CSVImporter
 {
 
@@ -17,69 +15,29 @@ class CSVPageImporter extends CSVImporter
      */
     protected function readHeaders()
     {
-
-        //add pid to struct
-        $pageType = new Page(null, 'pid');
-        $this->columns[] = new Column(0, $pageType, 0, true, $this->schema->getTable());
-
         parent::readHeaders();
-
         if (!in_array('pid', $this->header)) throw new StructException('There is no "pid" header in the CSV. Schema not imported.');
-    }
-
-    /**
-     * Creates the insert string for the single value table
-     *
-     * @return string
-     */
-    protected function getSQLforAllValues()
-    {
-        $colnames = array();
-        foreach ($this->columns as $i => $col) {
-            $colnames[] = 'col' . $col->getColref();
-        }
-        //replace first column with pid
-        $colnames[0] = 'pid';
-        //insert rev at the end
-        $colnames[] = 'rev';
-
-        $placeholds = join(', ', array_fill(0, count($colnames), '?'));
-        $colnames = join(', ', $colnames);
-        $table = $this->schema->getTable();
-
-        return "INSERT INTO data_$table ($colnames, latest) VALUES ($placeholds, 1)";
     }
 
     /**
      * Add the revision.
      *
      * @param string[] $values
-     * @param          $line
-     * @param string   $single
-     * @param string   $multi
      */
-    protected function saveLine($values, $line, $single, $multi)
+    protected function saveLine($values)
     {
         //create new page revision
         $pid = cleanID($values[0]);
         if ($this->createPage[$pid]) {
-            $this->createPage($pid, $line);
+            $this->createPage($pid, $values);
         }
-        $helper = plugin_load('helper', 'struct');
-        $revision = $helper->createPageRevision($pid, 'CSV data imported');
-        p_get_metadata($pid); // reparse the metadata of the page top update the titles/rev/lasteditor table
-
         // make sure this schema is assigned
         /** @noinspection PhpUndefinedVariableInspection */
         Assignments::getInstance()->assignPageSchema(
             $pid,
             $this->schema->getTable()
         );
-
-        //add page revision to values
-        $values[] = $revision;
-
-        parent::saveLine($values, $line, $single, $multi);
+        parent::saveLine($values);
     }
 
     /**
@@ -161,61 +119,6 @@ class CSVPageImporter extends CSVImporter
             },
             $text
         );
-    }
-
-    /**
-     * In the paga schemas primary key is a touple of (pid, rev)
-     *
-     * @param string[] $values
-     * @param string   $single
-     * @return array(pid, rev)
-     */
-    protected function insertIntoSingle($values, $single)
-    {
-        $pid = $values[0];
-        $rev = $values[count($values) - 1];
-
-        //update latest
-        $table = $this->schema->getTable();
-        $this->sqlite->query("UPDATE data_$table SET latest = 0 WHERE latest = 1 AND pid = ?", array($pid));
-
-        //insert into table
-        parent::insertIntoSingle($values, $single);
-
-        //primary key is touple of (pid, rev)
-        return array($pid, $rev);
-    }
-
-    /**
-     * Add pid and rev to insert query parameters
-     *
-     * @param string $multi
-     * @param string $pk
-     * @param string $column
-     * @param string $row
-     * @param string $value
-     */
-    protected function insertIntoMulti($multi, $pk, $column, $row, $value)
-    {
-        list($pid, $rev) = $pk;
-
-        //update latest
-        $table = $this->schema->getTable();
-        $this->sqlite->query("UPDATE multi_$table SET latest = 0 WHERE latest = 1 AND pid = ?", array($pid));
-
-        $this->sqlite->query($multi, array($pid, $rev, $column->getColref(), $row + 1, $value));
-    }
-
-    /**
-     * In page schemas we use REPLACE instead of INSERT to prevent ambiguity
-     *
-     * @return string
-     */
-    protected function getSQLforMultiValue()
-    {
-        $table = $this->schema->getTable();
-        /** @noinspection SqlResolve */
-        return "INSERT INTO multi_$table (pid, rev, colref, row, value, latest) VALUES (?,?,?,?,?,1)";
     }
 
     /**
