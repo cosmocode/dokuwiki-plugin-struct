544dca1b89f6be64f219a93762df0888844ac819
Anna Dabrowska <dabrowska@cosmocode.de>
2020-05-18 20:56:47 +0200

    Fix some incorrectly migrated lookup data

--- action/migration.php
+++ action/migration.php
@@ -152,13 +152,7 @@ class action_plugin_struct_migration extends DokuWiki_Action_Plugin
             }
 
             // introduce composite ids in lookup columns
-            $s = "SELECT C.colref AS COL, T.class AS TYPE
-                FROM schema_cols AS C
-                LEFT OUTER JOIN types AS T
-                    ON C.tid = T.id
-                WHERE C.sid = $sid
-                AND TYPE LIKE '%Lookup'
-            ";
+            $s = $this->getLookupColsSql($sid);
             $res = $sqlite->query($s);
             $cols = $sqlite->res2arr($res);
 
@@ -223,5 +217,137 @@ class action_plugin_struct_migration extends DokuWiki_Action_Plugin
         }
         $sqlite->query('COMMIT TRANSACTION');
         return true;
+    }
+
+    /**
+     * Executes Migration 17
+     *
+     * Fixes lookup data not correctly migrated by #16
+     * All lookups were presumed to reference lookup data, not pages, so the migrated value
+     * was always ["", <previous-pid-aka-new-rid>]. For page references it is ["<previous-pid>", 0]
+     *
+     * @param helper_plugin_sqlite $sqlite
+     * @return bool
+     */
+    protected function migration17(helper_plugin_sqlite $sqlite)
+    {
+        $sql = "SELECT MAX(id) AS id, tbl FROM schemas
+                    GROUP BY tbl
+            ";
+        $res = $sqlite->query($sql);
+        $schemas = $sqlite->res2arr($res);
+
+        $sqlite->query('BEGIN TRANSACTION');
+        $ok = true;
+
+        foreach ($schemas as $schema) {
+            // find lookup columns
+            $name = $schema['tbl'];
+            $sid = $schema['id'];
+            $s = $this->getLookupColsSql($sid);
+            $res = $sqlite->query($s);
+            $cols = $sqlite->res2arr($res);
+
+            if ($cols) {
+                $colnames = array_map(function ($c) {
+                    return 'col' . $c['COL'];
+                }, $cols);
+
+                // data_ tables
+                $s = 'SELECT pid, rid, rev, ' . implode(', ', $colnames) . " FROM data_$name";
+                $res = $sqlite->query($s);
+                $allValues = $sqlite->res2arr($res);
+
+                if (!empty($allValues)) {
+                    foreach ($allValues as $row) {
+                        list($pid, $rid, $rev, $colref, $rowno, $fixes) = $this->getFixedValues($row);
+                        // now fix the values
+                        if (!empty($fixes)) {
+                            $sql = "UPDATE data_$name SET " . implode(', ', $fixes) . " WHERE pid = ? and rid = ? AND rev = ?";
+                            $params = [$pid, $rid, $rev];
+                            $ok = $ok && $sqlite->query($sql, $params);
+                        }
+                    }
+                }
+
+                // multi_ tables
+                $s = "SELECT colref, pid, rid, rev, row, value FROM multi_$name";
+                $res = $sqlite->query($s);
+                $allValues = $sqlite->res2arr($res);
+
+                if (!empty($allValues)) {
+                    foreach ($allValues as $row) {
+                        list($pid, $rid, $rev, $colref, $rowno, $fixes) = $this->getFixedValues($row);
+                        // now fix the values
+                        if (!empty($fixes)) {
+                            $sql = "UPDATE multi_$name SET " . implode(', ', $fixes) . " WHERE pid = ? AND rid = ? AND rev = ? AND colref = ? AND row = ?";
+                            $params = [$pid, $rid, $rev, $colref, $rowno];
+                            $ok = $ok && $sqlite->query($sql, $params);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!$ok) {
+            $sqlite->query('ROLLBACK TRANSACTION');
+            return false;
+        }
+        $sqlite->query('COMMIT TRANSACTION');
+        return true;
+    }
+
+    /**
+     * Returns a select statement to fetch Lookup columns in the current schema
+     *
+     * @param int $sid Id of the schema
+     * @return string SQL statement
+     */
+    protected function getLookupColsSql($sid)
+    {
+        return "SELECT C.colref AS COL, T.class AS TYPE
+                FROM schema_cols AS C
+                LEFT OUTER JOIN types AS T
+                    ON C.tid = T.id
+                WHERE C.sid = $sid
+                AND TYPE LIKE '%Lookup'
+            ";
+    }
+
+    /**
+     * Checks for improperly migrated values and returns an array with
+     * "<column> = <fixed-value>" fragments to be used in the UPDATE statement.
+     *
+     * @param array $row
+     * @return array
+     */
+    protected function getFixedValues($row)
+    {
+        $pid = $row['pid'];
+        $rid = $row['rid'];
+        $rev = $row['rev'];
+        $colref = $row['colref'];
+        $rowno = $row['row'];
+        $fixes = [];
+        $matches = [];
+
+        // check if anything needs to be fixed in data columns
+        foreach ($row as $col => $value) {
+            if (in_array($col, ['pid', 'rid', 'rev', 'colref', 'row'])) {
+                continue;
+            }
+            preg_match('/^\["",(?<pid>.*?\D+.*?)\]$/', $value, $matches);
+            if (!empty($matches['pid'])) {
+                $fixes[$col] = '["' . $matches['pid'] . '",0]';
+            }
+        }
+
+        if (!empty($fixes)) {
+            $fixes = array_map(function ($set, $key) {
+                return "$key = '$set'";
+            }, $fixes, array_keys($fixes));
+        }
+
+        return [$pid, $rid, $rev, $colref, $rowno, $fixes];
     }
 }
