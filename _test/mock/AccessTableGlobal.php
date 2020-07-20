7b5b21b7f0f130db505c1aa668b70da3f2a092e5
Anna Dabrowska <dabrowska@cosmocode.de>
2020-07-20 17:20:35 +0200

    Test deleting all values when a multi field is updated inline
    This test has a very limited scope: only the save method, and it will catch
    very few errors in the inline editor. For that,
    \action_plugin_struct_inline must be refactored into more manageable
    public methods.

---
+++ _test/mock/AccessTableGlobal.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace dokuwiki\plugin\struct\test\mock;
+
+class AccessTableGlobal extends \dokuwiki\plugin\struct\meta\AccessTableGlobal {
+
+    public function getDataFromDB() {
+        return parent::getDataFromDB();
+    }
+}
