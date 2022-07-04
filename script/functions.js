addb381a40665e98e8fe2e0a1938d7c8e90ced96
Anna Dabrowska <dabrowska@cosmocode.de>
2022-07-04 21:41:31 +0200

    Simpler dot-safe selector

--- script/functions.js
+++ script/functions.js
@@ -67,7 +67,5 @@ function formatString(format) {
  * Custom onSelect handler for struct img button
  */
 window.insertStructMedia = function (edid, mediaid, opts, align) {
-    // escape dots in field id, bureaucracy plugin uses dots and it can break jQuery selectors
-    edid = edid.split('.').join('\\.');
-    jQuery('#' + edid).val(mediaid).change();
+    jQuery(document.getElementById(edid)).val(mediaid).change();
 };
