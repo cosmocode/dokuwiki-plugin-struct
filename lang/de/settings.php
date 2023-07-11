b1b0f0c8b525e39fa1acd5fe41c6b8bc1d87626a
Sevynidd <sevynidd02@protonmail.com>
2023-07-11 13:20:42 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -3,7 +3,9 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Sevynidd <sevynidd02@protonmail.com>
  */
-$lang['bottomoutput'] = 'Daten am Ende der Seite anzeigen';
-$lang['topoutput'] = 'Daten am Anfang der Seite anzeigen';
-$lang['disableDeleteSerial'] = 'Löschen von serial Daten deaktivieren';
+$lang['bottomoutput']          = 'Daten am Ende der Seite anzeigen';
+$lang['topoutput']             = 'Daten am Anfang der Seite anzeigen';
+$lang['disableDeleteSerial']   = 'Löschen von serial Daten deaktivieren';
+$lang['show_not_found']        = 'Zeige den Standard-Text wenn keine Ergebnisse für struct value syntax zurückgegeben wurden';
