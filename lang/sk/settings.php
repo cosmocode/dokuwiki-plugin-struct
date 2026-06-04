9fa5ef506d5973fdb60aa312e31e71eb0b367c50
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:25:44 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['bottomoutput']          = 'Zobraziť dáta v spodnej časti stránky';
+$lang['topoutput']             = 'Zobraziť dáta v hornej časti stránky';
+$lang['disableDeleteSerial']   = 'Vypnúť tlačidlo odstránenia pre sériové dáta';
+$lang['show_not_found']        = 'Zobraziť predvolený text, keď syntax struct hodnoty nevráti žiadne výsledky';
